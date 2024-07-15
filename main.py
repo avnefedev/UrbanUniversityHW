b3a815a7546e241f8ae96d899b904671676c8a26
avnefedev <106999837+avnefedev@users.noreply.github.com>
2024-07-15 23:01:45 +0300

    Add files via upload
    Выполнение вводного домашнего задания

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1st program
+
+print(9 ** 0.5 * 5)
+
+# 2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+
+print((1234 // 10) % 100 + (5678 // 10) % 100)
+
+# 4th program
+
+print(int(13.42) == int(42.13 % 42 * 100) or int(13.42 % 13 * 100) == int(42.13))
