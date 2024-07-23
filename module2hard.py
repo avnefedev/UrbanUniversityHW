dc6658fc292f1859773fd9bebc51fa6c40deca25
avnefedev <106999837+avnefedev@users.noreply.github.com>
2024-07-23 23:05:51 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,18 @@
+def key_for_req(n):
+    # n = int(input('Введите значение: '))
+    all_key = ''
+    def check_answer(a, b):
+        if n % (a + b) == 0:
+            return True
+
+    for i in range(1, n):
+        for j in range(i + 1, n):
+            flag = check_answer(i, j)
+            if flag:
+                all_key += str(i)
+                all_key += str(j)
+
+    print(f'Для числа {n} ответ - {all_key}')
+
+for i in range(3, 21):
+    key_for_req(i)
