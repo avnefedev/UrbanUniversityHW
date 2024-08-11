20d9de198aaf7fb112530c4261f036cabef293be
avnefedev <106999837+avnefedev@users.noreply.github.com>
2024-08-11 16:43:35 +0300

    Update and rename homework2.py to Module_1_HW/homework2.py

--- homework2.py
+++
@@ -1,6 +0,0 @@
-quantity_done_homework = 12
-spent_time = 1.5
-name_course = 'Python'
-time_for_one_task = spent_time / quantity_done_homework
-
-print(f'Курс: {name_course}, всего задач: {quantity_done_homework}, затрачено часов: {spent_time}, среднее время выполнения {time_for_one_task} часа.')
