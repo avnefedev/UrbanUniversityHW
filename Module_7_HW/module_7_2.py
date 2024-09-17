890760acb2e798df322c425ea41a0e1ced5c65b8
avnefedev <106999837+avnefedev@users.noreply.github.com>
2024-09-17 22:47:53 +0300

    Add files via upload

---
+++ Module_7_HW/module_7_2.py
@@ -0,0 +1,18 @@
+def custom_write(file_name, strings):
+    strings_positions = dict()
+    file = open(file_name, 'a', encoding='utf-8')
+    for i in range(len(strings)):
+        strings_positions[(i + 1, file.tell())] = strings[i]
+        file.write(f'{strings[i]}\n')
+    return strings_positions
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
