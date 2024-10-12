bf9b9e68eea012cdef0346d64cea9973f646ec0c
avnefedev <106999837+avnefedev@users.noreply.github.com>
2024-10-12 22:44:57 +0300

    Add files via upload

---
+++ Module_9_HW/module_9_7.py
@@ -0,0 +1,19 @@
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        count = 0
+        sum1 = func(*args, **kwargs)
+        for i in range(1, sum1 + 1):
+            if sum1 % i == 0:
+                count += 1
+        if count == 2:
+            return f'Простое\n{sum1}'
+        else:
+            return f'Составное\n{sum1}'
+    return wrapper
+
+@is_prime
+def sum_three(*args):
+    return sum(args)
+
+result = sum_three(3, 2, 6)
+print(result)
