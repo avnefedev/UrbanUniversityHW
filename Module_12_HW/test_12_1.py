46be097c9235f8acd78c2974cb0d3413ef880a18
avnefedev <106999837+avnefedev@users.noreply.github.com>
2024-11-17 22:29:15 +0300

    Update test_12_1.py

--- Module_12_HW/test_12_1.py
+++ Module_12_HW/test_12_1.py
@@ -1,13 +1,32 @@
-class Runner:
-    def __init__(self, name):
-        self.name = name
-        self.distance = 0
+from module_12_1 import Runner
+import unittest
 
-    def run(self):
-        self.distance += 10
 
-    def walk(self):
-        self.distance += 5
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        """Test walk"""
+        object = Runner('Вася')
+        for _ in range(10):
+            object.walk()
+        self.assertEqual(object.distance, 50)
 
-    def __str__(self):
-        return self.name
+    def test_run(self):
+        """Test run"""
+        object = Runner('Петя')
+        for _ in range(10):
+            object.run()
+        self.assertEqual(object.distance, 100)
+
+    def test_challenge(self):
+        object1, object2 = Runner('Вася'), Runner('Петя')
+        for _ in range(10):
+            object1.walk()
+            object2.run()
+        self.assertNotEquals(object1.distance, object2.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
