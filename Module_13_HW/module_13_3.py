d82fdfd3ada2b30748878e5e99b087f7dbb294a6
avnefedev <106999837+avnefedev@users.noreply.github.com>
2024-11-24 21:30:00 +0300

    Create module_13_3.py

---
+++ Module_13_HW/module_13_3.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from set import api
+
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(text=['Urban'])
+async def urban_message(message):
+    print(f'Urban message {message.message_id}')
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
